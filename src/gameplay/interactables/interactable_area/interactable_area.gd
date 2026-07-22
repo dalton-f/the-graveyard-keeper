9e9777070a7d8c958716ccc84478fce643c4cc6c
Dalton <daltonfiggdavies@gmail.com>
2026-07-22 11:26:23 +0100

    Fix overlapping and disabled interactables

--- src/gameplay/interactables/interactable_area/interactable_area.gd
+++ src/gameplay/interactables/interactable_area/interactable_area.gd
@@ -2,10 +2,15 @@ extends Area3D
 
 @onready var prompt: Label = $Prompt
 
+var interactables: Array[Interactable] = []
 var current_interactable: Interactable = null
 
 func _physics_process(_delta):
+	current_interactable = interactables[0] if interactables.size() > 0 else null
+	
 	if current_interactable:
+		prompt.text = current_interactable.get_prompt()
+		
 		# Check input every frame
 		if Input.is_action_just_pressed(current_interactable.prompt_action):
 			current_interactable.interact(owner)
@@ -13,11 +18,9 @@ func _physics_process(_delta):
 		prompt.text = ""
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is Interactable:
-		current_interactable = body
-		
-		prompt.text = current_interactable.get_prompt()
+	if body is Interactable and not interactables.has(body) and body.enabled:
+		interactables.append(body)
 
 func _on_body_exited(body: Node3D) -> void:
-	if body == current_interactable:
-		current_interactable = null
+	if body is Interactable:
+		interactables.erase(body)
